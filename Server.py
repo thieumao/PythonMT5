0c198d07b7264fcb2c9c917cfec56886006ab790
Thieu Mao <thieumao@gmail.com>
2021-04-01 01:07:20 +0700

    Create Server.py

---
+++ Server.py
@@ -0,0 +1,36 @@
+import socket
+import time
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(2)
+
+count = 0
+while True:
+	time.sleep(1)
+
+	client, addr = s.accept()
+
+	try:
+		print('Connected by', addr)
+		while True:
+			data = client.recv(1024)
+			str_data = data.decode("utf8")
+			if str_data == "quit":
+				break
+			"""if not data:
+				break
+			"""
+			count = count + 1
+			print(count)
+			print("Client: " + str_data)
+
+			# msg = input("Server: ")
+
+	finally:
+		client.close()
+
+s.close()
